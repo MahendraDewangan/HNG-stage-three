770a319413210fe7de5333ab327f9b0cd4b21486
Mahendra <mahendradewangan195@gmail.com>
2023-09-22 00:44:55 +0530

    implemented authorization feature using auth0

--- image-gallery/src/components/board/Board.jsx
+++ image-gallery/src/components/board/Board.jsx
@@ -1,79 +1,80 @@
-import React from 'react'
-import './Board.css'
+import React, { useState } from 'react';
+import './Board.css';
 import { Modal } from '../modal/Modal';
 import { Pins } from '../pin/Pins';
+import { useAuth0 } from "@auth0/auth0-react";
 
+const Board = () => {
+    const [pins, setPins] = useState([]);
+    const [showModel, setShowModel] = useState(false);
 
-class Board extends React.Component {
+    const addPin = (pinDetails) => {
+        setPins(prevPins => [
+            ...prevPins,
+            <Pins pinDetails={pinDetails} key={prevPins.length} />
+        ]);
+        setShowModel(false);
+    };
 
-    constructor(props) {
-        super(props);
+    const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
-        this.state = {
-            pins: [],
-            show_model: false
-        }
-    }
+    return (
+        <>
+            <header>
+                <div className="container flexSB">
+                    <nav className='flexSB navbar'>
+                        <div className="logo">
+                            <a href="/"><img src="./images/logo-rm.png" alt="" className="header_logo" /></a>
+                        </div>
+                        <div className="search-bar">
+                            <input type="text" placeholder='Search' />
+                            <i className="fa fa-search search-magnifying-glass" ></i>
+                        </div>
+                        <div className='flexSB menu'>
+                            <div className="menu-btn">
+                                <div className="logInOut">
+                                    {/* <a href="/"><i className="fa fa-user"></i></a> */}
+                                    {
+                                        isAuthenticated ? (
+                                            <div className="loggedOut">
+                                                <div class="dropdown">
+                                                    <button class="dropbtn"><i className="fa fa-user"></i></button>
+                                                    <div class="dropdown-content">
+                                                        <div className="userDetails" >
+                                                            {isAuthenticated && <p>{user.name}</p>}
+                                                        </div>
+                                                        
+                                                        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <div className="loggedIn">
+                                                <button onClick={() => loginWithRedirect()}>Log in</button>
+                                            </div>
+                                        )
 
-    add_pin = pinDetails => {
-        this.setState(_state => {
-            const new_pins = [
-                ..._state.pins
-            ]
-
-            new_pins.push(
-                <Pins pinDetails={pinDetails} key={_state.pins.length} />
-            )
-
-            return {
-                pins: new_pins,
-                show_model: false
-            }
-        })
-    }
-
-
-    render() {
-
-        return (
-
-            <>
-                <header >
-                    <div className="container flexSB">
-                        <nav className='flexSB navbar'>
-                            <div className="logo">
-                                <a href="/"><img src="./images/logo-rm.png" alt="" className="header_logo" /></a>
-
-                            </div>
-                            <div className="search-bar">
-                                <input type="text" placeholder='Search' />
-                                <i className="fa fa-search search-magnifying-glass" ></i>
-
-                            </div>
-                            <div className='flexSB menu'>
-                                <div className="menu-btn">
-                                    <a href="/"><i class="fa fa-user"></i></a>
+                                    }
                                 </div>
-                                <div onClick={() => this.setState({ show_model: true })} className="menu-btn">
-                                    <i class="fa fa-plus"></i>
-                                </div>
                             </div>
-                        </nav>
 
-                    </div>
-                </header>
-                <div className="pin_container">
-                    {this.state.pins}
+                            <div onClick={() => setShowModel(true)} className="menu-btn">
+                                <i className="fa fa-plus"></i>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-                <div onClick={event => event.target.className === 'add_pin_modal' ? this.setState({ show_model: false }) : null} className="add_pin_modal_container">
-                    {
-                        this.state.show_model ? <Modal add_pin={this.add_pin} /> : null
-                    }
+            </header>
+            <div className="pin_container">
+                {pins}
+            </div>
+            <div onClick={event => event.target.className === 'add_pin_modal' ? setShowModel(false) : null} className="add_pin_modal_container">
+                {
+                    showModel ? <Modal add_pin={addPin} /> : null
+                }
+            </div>
+        </>
+    );
+};
 
-                </div>
-            </>
-        )
-    }
-}
-
-export default Board
+export default Board;
--- image-gallery/src/index.js
+++ image-gallery/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <Auth0Provider
+    domain="dev-r5zk1vpi6n22nap8.us.auth0.com"
+    clientId="fjv5StxdFTvIFQizQ87EqsDfPORD5Vyh"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
   <App />
+  </Auth0Provider>
 
 );
-
